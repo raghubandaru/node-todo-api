f186802d19da53b3efd80eafaafc1857f4c816da
Raghu <raghubandaru45@gmail.com>
2018-03-18 13:16:33 +0530

    Setup separate production, development and test envs - database

---
+++ server/config/config.js
@@ -0,0 +1,13 @@
+var env = process.env.NODE_ENV || 'development';
+console.log('env *****************', env);
+
+if (env === 'production') {
+    process.env.MONGODB_URI = 'mongodb://admin:admin456@ds215759.mlab.com:15759/todoapp'
+}
+else if (env === 'development') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/todoappDev'
+} else if (env === 'test') {
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/todoappTest'
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://admin:admin456@ds215759.mlab.com:15759/todoapp`);
+mongoose.connect(process.env.MONGODB_URI);
 // mongoose.connect(`mongodb://localhost:27017/todoapp`);
 
 module.exports = { mongoose };
