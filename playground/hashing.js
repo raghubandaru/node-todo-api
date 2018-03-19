479b9ee3c8fd48f1c8512f9a7ae1997d71f927d3
Raghu <raghubandaru45@gmail.com>
2018-03-19 17:08:27 +0530

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,30 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-    id: 10
-};
+var password = '123456!';
 
-var token = jwt.sign(data, 'raghuBandaru');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
 
-var decoded = jwt.verify(token, 'raghuBandaru');
-console.log(decoded);
+var hashedPwd = '$2a$10$.dw11QQq7XyMOgblll/EXu12moifho3DOkSSNR09i/7Vw2j3wbAnK';
+
+bcrypt.compare('password', hashedPwd, (err, res) => {
+    console.log(res);
+});
+
+// var data = {
+//     id: 10
+// };
+
+// var token = jwt.sign(data, 'raghuBandaru');
+// console.log(token);
+
+// var decoded = jwt.verify(token, 'raghuBandaru');
+// console.log(decoded);
 
 // var message = 'plain text';
 // var hash = SHA256(message).toString();
